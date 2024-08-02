f415a2cac0cf5b19848cbc95eeb4540b089cb292
forward <yangyangmyo1994@gmail.com>
2024-08-03 01:00:40 +0800

    Merge remote-tracking branch 'origin/master'

--- install-docker.sh
+++ install-docker.sh
@@ -9,7 +9,7 @@ docker-logrotate \
 docker-engine
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker run hello-world
